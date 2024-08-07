d695633147ea83f007fee6d311946348148511f7
Константин Кара <98524653+AIkpuk@users.noreply.github.com>
2024-08-07 21:46:22 +0300

    Add files via upload

---
+++ factory/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace factory
+{
+    public partial class Form1 : Form
+    {
+        List<User> users = new List<User>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            int y = 0;
+            string v;
+
+            string login = "";
+            string password = "";
+            User user = new User();
+
+            var result = users.Where(x => x.UserName == login && x.Password == password).FirstOrDefault();
+
+
+
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>
+            {
+                "qqq",
+                "ewq"
+            };
+
+
+            dataGridView1.DataSource = list;
+        }
+    }
+}
